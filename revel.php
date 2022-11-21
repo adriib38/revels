4871b731f423e2cf2dc271ba26c135e3559688ee
Adrián <39964431+adriib38@users.noreply.github.com>
2022-11-21 12:33:13 +0100

    21/11/2022
    Colores arreglados .css

--- revel.php
+++ revel.php
@@ -24,8 +24,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Poppins:wght@500;600&display=swap" rel="stylesheet"> 
-
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700;800&family=Montserrat:wght@300&family=Poppins:wght@500;600&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="styles\style.css">
 </head>
     <body>
